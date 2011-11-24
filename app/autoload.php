a746b5d0390150e66ab8d7fd4919dd9de695750a
Klein Florian <florian.klein@free.fr>
2011-11-24 16:08:38 +0100

    update default config to work with latest symfony

--- app/autoload.php
+++ app/autoload.php
@@ -8,16 +8,16 @@ $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
-    'Assetic'          => __DIR__.'/../vendor/assetic/src',
-    'Sensio'           => __DIR__.'/../vendor/bundles',
-    'JMS'              => __DIR__.'/../vendor/bundles',
-    'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
-    'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
-    'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
+    //'Assetic'          => __DIR__.'/../vendor/assetic/src',
+    //'Sensio'           => __DIR__.'/../vendor/bundles',
+    //'JMS'              => __DIR__.'/../vendor/bundles',
+    //'Doctrine\\Common' => __DIR__.'/../vendor/doctrine-common/lib',
+    //'Doctrine\\DBAL'   => __DIR__.'/../vendor/doctrine-dbal/lib',
+    //'Doctrine'         => __DIR__.'/../vendor/doctrine/lib',
 ));
 $loader->registerPrefixes(array(
-    'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
+    //'Twig_Extensions_' => __DIR__.'/../vendor/twig-extensions/lib',
     'Twig_'            => __DIR__.'/../vendor/twig/lib',
-    'Swift_'           => __DIR__.'/../vendor/swiftmailer/lib/classes',
+    //'Swift_'           => __DIR__.'/../vendor/swiftmailer/lib/classes',
 ));
 $loader->register();
