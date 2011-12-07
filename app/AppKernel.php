fb91a7ceeb5598aba146811e0da546e9f3cdfd00
Klein Florian <florian.klein@free.fr>
2011-12-07 10:43:20 +0100

    comment AsseticBundle by default

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -12,7 +12,7 @@ class AppKernel extends Kernel
             new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\TwigBundle\TwigBundle(),
-            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            //new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             //new Symfony\Bundle\SecurityBundle\SecurityBundle(),
             //new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             //new Symfony\Bundle\DoctrineBundle\DoctrineBundle(),
